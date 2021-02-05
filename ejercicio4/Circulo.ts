321d4e27f57b3dd085c22385c9d7c3d0dbc9f739
Andy-M7 <allataspariguana@gmail.com>
2021-02-05 16:57:46 -0500

    Clase circulo creada

--- ejercicio4/Circulo.ts
+++ ejercicio4/Circulo.ts
@@ -1,13 +1,21 @@
 class Circulo{
-    propiedad1: string;
-    propiedad2: string;
-    constructor(propiedad1: string, propiedad2: string){
-        this.propiedad1 = propiedad1
-        this.propiedad2 = propiedad2
+    radio: Double;
+    color: string;
+
+    constructor(){          //constructor sin parametros
+        
     }
-    dato(){
-        console.log(`Propiedad1: ${this.propiedad1}, Propiedad2: ${this.propiedad2}`)
+
+    constructor(radio: Double){  // constructor con parametro Radio
+        this.radio = radio
+    }
+
+    getradio(){
+        console.log(``)
+    }
+
+    getcolor(){
+        console.log(``)
     }
 }
 
-let circulo1 = new Circulo('done1','done2');
